548029236a5a986dcb213bf8f02c0ee3411e941a
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-01 22:36:52 +0100

    cast as integer in cents

--- greedy.c
+++ greedy.c
@@ -9,5 +9,7 @@ int main(void)
     printf("Hey! How much change is owed?\n");
     //get a floating point number
     float dollars = GetFloat();
+    // cast as integer in cents
+    int cents = (int)round(100*dollars);
 }
 
