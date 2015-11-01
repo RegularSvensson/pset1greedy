c3504d67c8309ff1bfa1c25c0cc823ab37a63212
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-01 22:36:44 +0100

    get dollars

--- greedy.c
+++ greedy.c
@@ -8,7 +8,6 @@ int main(void)
     // print prompt to user
     printf("Hey! How much change is owed?\n");
     //get a floating point number
-    float change = GetFloat();
-    printf("Your change is: %f\n", change);
+    float dollars = GetFloat();
 }
 
