d6372803394f4dbf735e6bf7e2b0d0b626831ca1
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-02 11:41:33 +0100

    Add comments

--- greedy.c
+++ greedy.c
@@ -24,21 +24,28 @@ int main(void)
     // initalize coins to 0
     int coins = 0;
     
+    // count quarters
     if (cents >= 25)
     {
         coins += cents / 25;
         cents %= 25;
     }
+    
+    // count dimes
     if (cents >= 10)
     {
         coins += cents / 10;
         cents %= 10;
     }
+    
+    // count nickels
     if (cents >= 5)
     {
         coins += cents / 5;
         cents %= 5;
     }
+    
+    // count pennies
     if (cents >= 1)
     {
         coins += cents / 1;
